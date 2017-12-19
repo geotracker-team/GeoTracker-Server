ea9e7803417b77b227cd86ddd5d917b26479244c
Xavier <xplanes123@gmail.com>
2017-12-20 00:19:44 +0100

    - API web service test code

--- GeoTrackerWeb/src/webService/WebService.java
+++ GeoTrackerWeb/src/webService/WebService.java
@@ -1,5 +1,6 @@
 package webService;
 
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -22,35 +23,90 @@ import api.ProjectsAPI;
 @Consumes({ "application/xml", "application/json" })
 public class WebService {
 
-		
+	@GET
+	@Path("/login/{name}/{pass}")
+	public boolean login (@PathParam("name") String name, @PathParam("pass") String pass){
+		System.out.println("login");
+		return true;
+	}	
+	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/projects/{name}")
-	public Project[] getProjectsOfUser(@PathParam("name") String name) throws SQLException	
+	@Path("/projects/{name}/{pass}")
+	public Project[] getProjectsOfUser(@PathParam("name") String name, @PathParam("pass") String pass) throws SQLException	
 	{
-		ProjectsAPI api = new ProjectsAPI();
-		ArrayList<Project> projects = api.getAllProjectsByUser(name);	
+		/*ProjectsAPI api = new ProjectsAPI();
+		ArrayList<Project> projects = api.getAllProjectsByUser(name);	*/
+		
+		//TEST CODE
+		ArrayList<Project> projects = new ArrayList<>();
+				
+		Project project = new Project();
+		project.setName("project test");
+		project.setId(1);
+		project.setIdCompany(1);
+		projects.add(project);
+		
+		Project project2 = new Project();
+		project2.setId(2);
+		project2.setName("project test 2");
+		project2.setIdCompany(2);
+		projects.add(project2);		
 		
 		return projects.toArray(new Project[0]);
 	}	
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/records/{name}")
-	public Record[] getRecordsOfUser(@PathParam("name") String name) throws SQLException	
+	@Path("/records/{name}/{pass}/{idProject}")
+	public Record[] getRecordsOfProject(@PathParam("name") String name, @PathParam("pass") String pass, @PathParam("idProject") int idProject) throws SQLException	
 	{
-		API api = new API();
-		ArrayList<Record> projects = api.getAllRegistersByUserName(name);	
+		/*API api = new API();
+		ArrayList<Record> projects = api.getAllRegistersByUserName(name);	*/
 		
-		return projects.toArray(new Record[0]);
+		//TEST CODE
+		ArrayList<Record> records = new ArrayList<>();
+		
+		Record record = new Record();
+		record.setId(1);
+		record.setDescription("rest service project description test 1");
+		record.setDate("17/12/2017");
+		record.setIdUser(1);
+		record.setIdProject(1);
+		record.setLatitude(1);
+		record.setLongitude(1);	
+		records.add(record);
+		
+		Record record2 = new Record();
+		record2.setId(2);
+		record2.setDescription("rest service project description test 2");
+		record2.setDate("17/13/2017");
+		record2.setIdUser(2);
+		record2.setIdProject(2);
+		record2.setLatitude(-9999);
+		record2.setLongitude(9999);	
+		records.add(record2);
+		
+		Record record3 = new Record();
+		record3.setId(3);
+		record3.setDescription("rest service project description test 3");
+		record3.setDate("17/14/2017");
+		record3.setIdUser(3);
+		record3.setIdProject(2);
+		record3.setLatitude(-3333);
+		record3.setLongitude(3333);	
+		records.add(record3);
+			
+		return records.toArray(new Record[0]);
 	}
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	@Path("/record")
-	public String addRegister() throws SQLException	
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/addRecord/{name}/{pass}")
+	public boolean addRecord(@PathParam("name") String name, @PathParam("pass") String pass, InputStream incomingData) throws SQLException	
 	{
-		Record record = new Record();
+		/*Record record = new Record();
 		//record.setId(resultSet.getInt(1));
 		record.setDescription("rest service description test");
 		record.setDate("17/12/2017");
@@ -60,10 +116,22 @@ public class WebService {
 		record.setLongitude(1);	
 						
 		API api = new API();
-		api.createRegister(record);;	
+		api.createRegister(record);;	*/
+		System.out.println("addRecord");
 		
-		return "Record added succesfully";
+		return true;
 	}	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/editRecord/{name}/{pass}/{idRecord}")
+	public boolean editRecord(@PathParam("name") String name, @PathParam("pass") String pass,
+			@PathParam("idRecord") int idRecord, InputStream incomingData){
+		System.out.println("editRecord");
+		return true;
+	}
+	
 
 	/*@GET
 	@Produces(MediaType.TEXT_PLAIN)
